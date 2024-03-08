2a085988fcd8a8cc4cd3a363938df9001f8bc4b6
joaovictorpeix <joaovictor.dias1203@gmail.com>
2024-03-08 16:23:04 -0300

    Atualizações: Tela de dados do paciente

--- cuidarmais/lib/pages/password_recovery/password_recovery.dart
+++ cuidarmais/lib/pages/password_recovery/password_recovery.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cuidarmais/pages/PatientDataManagementPage/PatientDataManagementPage.dart'; // Importe o arquivo PatientDataManagementPage.dart
 
 class PasswordRecoveryPage extends StatelessWidget {
   const PasswordRecoveryPage({Key? key}) : super(key: key);
@@ -7,7 +8,25 @@ class PasswordRecoveryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recuperação de Senha'), // Título da página
+        automaticallyImplyLeading: false, // Remover o botão de voltar padrão
+        backgroundColor: Color.fromARGB(255, 24, 147, 189),
+        titleSpacing: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                'assets/TesteBar.png', // Caminho para o logotipo
+                height: 40, // Altura do logotipo
+              ),
+            ),
+            Text(
+              'CUIDAR +', // Texto ao lado do logotipo
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -16,15 +35,22 @@ class PasswordRecoveryPage extends StatelessWidget {
           children: [
             Text(
               'Insira seu e-mail para recuperar a senha:', // Instrução para o usuário
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 20),
             ),
             const SizedBox(height: 20), // Espaçamento entre elementos
 
             // Campo de entrada de e-mail
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-                hintText: 'Digite seu e-mail',
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 20), // Margens personalizadas
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  hintText: 'Digite seu e-mail',
+                  prefixIcon: Icon(Icons.email_outlined,
+                      color: Color.fromARGB(255, 2, 84,
+                          109)), // Ícone de e-mail com cor personalizada
+                ),
               ),
             ),
             const SizedBox(height: 20), // Espaçamento entre elementos
@@ -35,7 +61,22 @@ class PasswordRecoveryPage extends StatelessWidget {
                 // Lógica para enviar o e-mail de recuperação de senha
                 // Aqui você implementaria a lógica para enviar o e-mail de recuperação
               },
-              child: const Text('Enviar'),
+              child: const Text('Recuperar Senha'),
+            ),
+
+            const SizedBox(height: 20), // Espaçamento entre elementos
+
+            // Botão para gerenciar os dados do paciente
+            ElevatedButton(
+              onPressed: () {
+                // Navegação para a tela de gerenciamento de dados do paciente
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PatientDataManagementPage()),
+                );
+              },
+              child: const Text('Gerenciar Dados do Paciente'),
             ),
           ],
         ),
