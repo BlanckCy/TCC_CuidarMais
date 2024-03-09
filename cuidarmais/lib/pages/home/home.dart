3c5cf992ae591de2c43e505e96636d8c0991857e
Ana <anapaulabrandi@yahoo.com>
2024-03-09 14:21:31 -0300

    deixar app bar separado

--- cuidarmais/lib/pages/home/home.dart
+++ cuidarmais/lib/pages/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:cuidarmais/widgets/customAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:cuidarmais/models/database.dart';
 
@@ -9,46 +10,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<dynamic> _buscarCuidadores() async {
-    try {
-      return await Database.buscarDadosGet('/cuidadores/lista', {});
-    } catch (e) {
-      // Tratamento de erro
-      print('Erro ao buscar cuidadores: $e');
-      return null;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        leading: Icon(Icons.menu),
-        title: Text("App"),
-      ),
-      body: FutureBuilder<dynamic>(
-        future: _buscarCuidadores(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError || snapshot.data == null) {
-            return Center(child: Text('Erro ao carregar cuidadores'));
-          } else {
-            final cuidadores = snapshot.data;
-            return ListView.builder(
-              itemCount: cuidadores.length,
-              itemBuilder: (context, index) {
-                final cuidador = cuidadores[index];
-                return ListTile(
-                  title: Text('Nome: ${cuidador['nome']}'),
-                  subtitle: Text('Email: ${cuidador['email']}'),
-                );
-              },
-            );
-          }
-        },
-      ),
+      appBar: const CustomAppBar(),
     );
   }
 }
