a320d2758bd49df035dff3b32cd83c126b22b497
Ana <anapaulabrandi@yahoo.com>
2024-03-29 18:02:41 -0300

    adicionado menu

---
+++ cuidarmais/lib/widgets/customBottomBar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          label: 'Registrar Rotina',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_filled),
+          label: 'Iniciar',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.local_hospital),
+          label: 'Botão Emergência',
+        ),
+      ],
+      currentIndex: widget.selectedIndex,
+      selectedItemColor: Colors.blue,
+      onTap: widget.onItemTapped,
+    );
+  }
+}
